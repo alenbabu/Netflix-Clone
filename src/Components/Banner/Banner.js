547fd89f447277ea5826dcd087d0cef51a7c2f62
Alen Babu <alenbabualen@gmail.com>
2023-10-30 12:49:35 +0530

    navbar updated,     banner content div added - logo title added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='banner-content'>
+            <div className='heading'>
+                <img className='heading-logo' src="./assets/banner-logo.png" alt=""/>
+                <h4>SERIES</h4><br/>
+                <h1>MONEY HEIST</h1>
+                
+
+            </div>
+            <div className='banner-description'>
+
+            </div>
+            <div className='buttons'>
+
+            </div>
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+
+        <div className="navbar">
+            <img className='logo' src='./assets/logo.png' alt='logo' />
+            <img className='avatar' src='./assets/avatar.png' alt='avatar' />
+
+        </div>
+
+
+
+
+    )
+}
+
+export default Navbar
