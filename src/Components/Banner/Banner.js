2d8a497a70f6a762e287c67ae1c702d22aa86a35
Alen Babu <alenbabualen@gmail.com>
2023-10-31 11:50:57 +0530

    banner content and design modified

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -11,17 +11,16 @@ function Banner() {
             </div>
             <div className='banner-heading-content'>
                 <h1 className='banner-heading'>MONEY HEIST</h1>
-                <p className='banner-description'>Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan</p>
+                <h4 className='banner-description'>Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan</h4>
 
             </div>
-            <div className='buttons'>
+            <div className='banner-buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>More info</button>
 
             </div>
-
-
         </div>
+        <div className='fade-bottom'></div>
 
     </div>
   )
