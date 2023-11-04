8ffc10e08258cba6493d4c1c6c1949955e59b3a2
Alen Babu <alenbabualen@gmail.com>
2023-11-04 15:43:53 +0530

    react-player npm installed,connected movies to their own trailer using tmdb movie api . Working

--- src/Components/Posters-Row/Postersrow.js
+++ src/Components/Posters-Row/Postersrow.js
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import './postersrow.css'
 import axios from '../../constants/axios';
-import { imageUrl } from '../../constants/constants';
+import { API_KEY, imageUrl } from '../../constants/constants';
+import ReactPlayer from 'react-player/youtube';
 
 function Postersrow(props) {
     const [poster, setPoster] = useState([]);
+    const [urlId, setUrlId] = useState('');
+    
     
     useEffect(() => {
         axios.get(props.url).then((Response)=>{
-            console.log(Response.data);
             setPoster(Response.data.results)
             
         })     
     }, []);
+
+    const handleMouseClick=(id)=>{
+        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((Response)=>{
+            setUrlId(Response.data.results[0].key)
+            
+        })
+        
+    }
+
+    const link=`https://www.youtube.com/watch?v=${urlId}`
+
     
     return (
         <div className='row'>
             <h2>{props.title}</h2>
             <div className='posters'>
                 {
-                    poster.map((obj)=><img className={props.isSmall ? 'smallposter' :'poster'} src={`${imageUrl+obj.backdrop_path}`} alt='' />
+                    poster.map((obj)=><img onClick={()=>handleMouseClick(obj.id)} className={props.isSmall ? 'smallposter' :'poster'} src={`${imageUrl+obj.backdrop_path}`} alt='poster'/>
 
                     )
                 }
         
             </div>
-
+            {urlId ? <ReactPlayer urlId={link} width='100%' height='35vh' controls={true}/> : ""}
         </div>
     )
 }
